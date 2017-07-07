ba94923394c8f2e003b15a6e29f819dbc4309b27
Erick <erick_aug@hotmail.com>
2017-07-07 20:48:48 -0300

    Criação do main

---
+++ src/CarroFuzzy/CarroFuzzy.java
@@ -0,0 +1,9 @@
+package CarroFuzzy;
+
+public class CarroFuzzy {
+    public static void main(String[] args) {
+        //Deixa o JFrame visível para ser executado com os parâmetros passados pelo usuário.
+        Sensores sensor = new Sensores();
+        sensor.setVisible(true);
+    }
+}
